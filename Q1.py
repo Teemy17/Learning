ac2617fdd6f0ee8b48b8f403461a61d8b4d7cae9
Pannawat Yorkhant <113030120+Teemy17@users.noreply.github.com>
2023-07-13 20:02:52 +0700

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,11 @@
+n_1 = float(input("Input first number:"))
+n_2 = float(input("Input second number:"))
+n_3 = float(input("Input third number:"))
+n_4 = float(input("Input Number 4:"))
+n_5 = float(input("Input Number 5:"))
+
+sum = n_1 + n_2 + n_3 + n_4 + n _5
+avg = sum / 5
+
+print("The sum is", sum)
+print("The average is", avg)
