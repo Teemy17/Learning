283c41a2667355064ddcfd6f4ed03a1f3b410494
Teemy17 <Teemy17@gmail.com>
2023-07-20 21:19:28 +0700

    edited

--- Intro computer programming/Q1.py
+++ Intro computer programming/Q1.py
@@ -4,7 +4,7 @@ n_3 = float(input("Input third number:"))
 n_4 = float(input("Input Number 4:"))
 n_5 = float(input("Input Number 5:"))
 
-sum = n_1 + n_2 + n_3 + n_4 + n _5
+sum = n_1 + n_2 + n_3 + n_4 + n_5
 avg = sum / 5
 
 print("The sum is", sum)
--- Intro computer programming/Q2.py
+++ Intro computer programming/Q2.py
@@ -1,6 +1,6 @@
 import math
-Rad = float(input("Enter radius:"))
-Len = float(input("Enter length:"))
+Rad = float(input("Enter radius: "))
+Len = float(input("Enter length: "))
 Area = math.pi * Rad**2
 Volume = Area * Len
 print("Area =", Area)
