dae3cf4328c655633bfc0b295ad0a183b0a82569
Teemy17 <Teemy17@gmail.com>
2023-07-22 16:15:23 +0700

    kk

---
+++ Intro computer programming/HW03/Q3.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Turtle()
+
+Len = float(input("Enter the length: "))
+
+t.lt(72)
+t.fd(Len)
+t.rt(144)
+t.fd(Len)
+t.rt(144)
+t.fd(Len)
+t.rt(144)
+t.fd(Len)
+t.rt(144)
+t.fd(Len)
+t.rt(144)
+
+turtle.done()
