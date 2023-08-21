c5d7f9dbe553baa05d8093fff966fb59b66f70b2
Pannawat Yorkhant <113030120+Teemy17@users.noreply.github.com>
2023-08-21 16:57:15 +0700

    Added another version

--- Intro computer programming/HW06/Q1.py
+++ Intro computer programming/HW06/Q1.py
@@ -8,4 +8,15 @@ def time_24_to_12(time) :
 
 print(time_24_to_12("23:24"))
 
+# Another version using if else.
 
+ def time_24_to_12_2(time) :
+     hours, minute = time.split(":")
+     if int(hours) > 12:
+         return (f"{int(hours) - 12}:{minute} PM")
+     elif int(hours) >= 12:
+         return (f"{int(hours)}:{minute} PM")
+     else:
+         return (f"{int(hours)}:{minute} AM")
+
+ print(time_24_to_12_2("23:24"))
